2adf9cb2c495836696853ce3878d2dbe8f0eeaea
Daniel Lam (MacBook) <daniel@lamophone.com>
2013-05-06 08:24:51 +1000

    Initial Commit

---
+++ SydneyChristianEvents/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SydneyChristianEvents
+//
+//  Created by Daniel Lam on 6/05/13.
+//  Copyright (c) 2013 Lamophone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SydneyChristianEvents/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SydneyChristianEvents
+//
+//  Created by Daniel Lam on 6/05/13.
+//  Copyright (c) 2013 Lamophone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
